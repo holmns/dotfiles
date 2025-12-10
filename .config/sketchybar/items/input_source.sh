42f23376450f2366a3e501dc43107936a6884430
Nawat Suangburanakul <nawathome@gmail.com>
2025-12-10 16:51:59 +0700

    optimized input_source

--- .config/sketchybar/items/input_source.sh
+++ .config/sketchybar/items/input_source.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 sketchybar --add item input_source right
+sketchybar --add event input_change "AppleSelectedInputSourcesChangedNotification"
 sketchybar --set input_source \
     label.font="$FONT:Semibold:14.0" \
-    script="$PLUGIN_DIR/get_input_source.sh" \
     label.color=$BLACK \
     background.color=$ICON_COLOR \
     background.height=20 \
     background.border_radius=6 \
     label.shadow.drawing=off \
-    update_freq=1 
+    script="$PLUGIN_DIR/get_input_source.sh" \
+    --subscribe input_source input_change
 
 
